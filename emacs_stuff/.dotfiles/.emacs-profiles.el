191b21eba7c92c9b7967512b48f49f5f2e4b0f01
benshurts <benjaminshurts@gmail.com>
2022-04-26 17:58:59 -0600

    added emacs source

---
+++ emacs_stuff/.dotfiles/.emacs-profiles.el
@@ -0,0 +1,7 @@
+
+(("default"   . ((user-emacs-directory   . "~/.dotfiles/.emacs.d")))
+ ("doom"      . ((user-emacs-directory   . "~/emacs-configs/doom-emacs")
+                 (env . (("DOOMDIR"      . "~/.dotfiles/.doom.d")))))
+ ("spacemacs" . ((user-emacs-directory   . "~/emacs-configs/spacemacs")
+                 (env . (("SPACEMACSDIR" . "~/.dotfiles/.spacemacs.d"))))) 
+ ("efs" . ((user-emacs-directory . "~/emacs-configs/emacs-from-scratch"))))
--- emacs_stuff/chemacs_as_default/.emacs-profiles.el
+++ emacs_stuff/chemacs_as_default/.emacs-profiles.el
@@ -1,3 +1,7 @@
-(("default". ((user-emacs-directory . "~/.emacs.d"))))
+(("default". ((user-emacs-directory . "~/.emacs.d")))
+ ("doom" . ((user-emacs-directory   . "~/emacs-configs/doom-emacs")
+ 	    (env . (("DOOMDIR"      . "~/.dotfiles/.doom.d")))))
+ ("spacemacs" . ((user-emacs-directory . "~/emacs-configs/spacemacs")
+ 		 (env . (("SPACEMACSDIR" . "~/.dotfiles/.spacemacs.d"))))))
 
 
